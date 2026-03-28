a1b049e2cfcfea94bae4d904dfe2c8d3519698dd
lixixi89055465 <nanji1850094299@outlook.com>
2026-03-28 23:27:55 +0800

    init

--- src/lc01/16多模态聊天机器人-7.py
+++ src/lc01/16多模态聊天机器人-7.py
@@ -1,6 +1,7 @@
 # 提示词模板
 import uuid
 
+from gradio.themes.app import chatbot
 from langchain_community.chat_message_histories import SQLChatMessageHistory, ChatMessageHistory
 from langchain_core.messages import HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
@@ -17,6 +18,9 @@ prompt = ChatPromptTemplate.from_messages([
 
 chain = prompt | multiModal_llm
 
+def get_last_user_after_assistant(history):
+    pass
+
 
 def get_session_history(session_id: str):
     ''' 从关系型数据库的历史消息列表中 返回当前会话 的所有历史小i下'''
@@ -26,15 +30,48 @@ def get_session_history(session_id: str):
     )
 
 
+def submit_messages(history):
+    '''提交用户输入的消息，生成机器人回复'''
+    user_messages = get_last_user_assistant(history)
+    print(user_messages)
+
+
 chain_history = RunnableWithMessageHistory(
     runnable=chain,
     get_session_history=get_session_history,
 )
-
-# user_msg = HumanMessage(content=[{'type': 'text', 'content': '你知道机器学习是什么意思吗'}])
-# user_msg = "5除以4等于几"
-user_msg = HumanMessage(content=[{'type': 'text', 'text': '你知道机器学习是什么意思吗'}])
 config = {'configurable': {'session_id': str(uuid.uuid4())}}
-resp1 = chain_history.invoke({'messages': [user_msg]}, config)
-print('1' * 100)
-print(resp1.content)
+
+
+def add_messages(history, messages):
+    ''' 将用户输入的消息添加到聊天记录中'''
+    for m in messages['files']:
+        print(m)
+        history.append({'role': 'user', 'content': {'path': m}})
+    # 处理文本消息
+    if messages['text'] is not None:
+        history.append({'role': 'user', 'content': {'text': messages['text']}})
+    return history, gr.MultimodalTextbox(value=None, interactive=False)  # 返回更新后的历史和重置的输入框
+
+
+with gr.Blocks(title='多模态聊天机器人', theme=gr.themes.Soft()) as block:
+    # 聊天历史记录的组件
+    chatbot = gr.Chatbot(type='messages', height=500, label='聊天机器人', bubble_full_width=False)
+    # 创建多模态输入框
+    chat_input = gr.MultimodalTextbox(
+        interactive=True,  # 可交互
+        file_types=['image', '.wav', '.mp4'],
+        file_count='multiple',  # 允许多文件上传
+        placeholder='请输入信息或者上传文件...',  # 输入框提升文本
+        show_label=False,  # 不显示标签
+        sources=['microphone', 'upload'],  # 支持麦克风和文件上传
+    )
+    chat_input.submit(
+        add_messages,
+        [chatbot, chat_input],
+        [chatbot, chat_input]
+    ).then(
+        submit_messages,
+        [chatbot],
+        [chatbot]
+    )
