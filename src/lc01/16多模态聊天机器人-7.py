100452f69d65f46110100ea4697aca2f685f60a0
lixixi89055465 <nanji1850094299@outlook.com>
2026-03-30 00:00:18 +0800

    init

--- src/lc01/16多模态聊天机器人-7.py
+++ src/lc01/16多模态聊天机器人-7.py
@@ -1,9 +1,7 @@
 # 提示词模板
 import uuid
 
-from gradio.themes.app import chatbot
-from langchain_community.chat_message_histories import SQLChatMessageHistory, ChatMessageHistory
-from langchain_core.messages import HumanMessage
+from langchain_community.chat_message_histories import SQLChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables import RunnableWithMessageHistory, RunnablePassthrough
 
@@ -18,8 +16,23 @@ prompt = ChatPromptTemplate.from_messages([
 
 chain = prompt | multiModal_llm
 
+
 def get_last_user_after_assistant(history):
-    pass
+    '''反向便利找到最后一个assistant的位置，并返回后面的所有user消息'''
+    if not history:
+        return None
+    if history[-1]['role'] == 'assistant':
+        return None
+    last_assistant_idx = -1
+    for i in range(len(history) - 1, -1, -1):
+        if history[i]['role'] == 'assistant':
+            last_assistant_idx = i
+            break
+    # 如果没有找到assistant
+    if last_assistant_idx == -1:
+        return history
+    else:
+        return history[last_assistant_idx + 1:]
 
 
 def get_session_history(session_id: str):
@@ -30,9 +43,23 @@ def get_session_history(session_id: str):
     )
 
 
+def add_message(chat_history, user_message):
+    ''' 将用户输入的消息添加到聊天记录中'''
+    # for m in user_message['files']:
+    #     print(m)
+    #     chat_history.append({'role': 'user', 'content': {'path': m}})
+    # # 处理文本消息
+    # if user_message['text'] is not None:
+    #     chat_history.append({'role': 'user', 'content': {'text': user_message['text']}})
+    # return chat_history, gr.MultimodalTextbox(value=None, interactive=False)  # 返回更新后的历史和重置的输入框
+    if user_message:
+        chat_history.append({'role': 'user', 'content': user_message})
+    return chat_history, gr.Textbox(value=None, interactive=False)  # 返回更新后的历史和重置的输入框
+
+
 def submit_messages(history):
     '''提交用户输入的消息，生成机器人回复'''
-    user_messages = get_last_user_assistant(history)
+    user_messages = get_last_user_after_assistant(history)
     print(user_messages)
 
 
@@ -41,18 +68,6 @@ chain_history = RunnableWithMessageHistory(
     get_session_history=get_session_history,
 )
 config = {'configurable': {'session_id': str(uuid.uuid4())}}
-
-
-def add_messages(history, messages):
-    ''' 将用户输入的消息添加到聊天记录中'''
-    for m in messages['files']:
-        print(m)
-        history.append({'role': 'user', 'content': {'path': m}})
-    # 处理文本消息
-    if messages['text'] is not None:
-        history.append({'role': 'user', 'content': {'text': messages['text']}})
-    return history, gr.MultimodalTextbox(value=None, interactive=False)  # 返回更新后的历史和重置的输入框
-
 
 with gr.Blocks(title='多模态聊天机器人', theme=gr.themes.Soft()) as block:
     # 聊天历史记录的组件
@@ -67,7 +82,7 @@ with gr.Blocks(title='多模态聊天机器人', theme=gr.themes.Soft()) as bloc
         sources=['microphone', 'upload'],  # 支持麦克风和文件上传
     )
     chat_input.submit(
-        add_messages,
+        add_message,
         [chatbot, chat_input],
         [chatbot, chat_input]
     ).then(
